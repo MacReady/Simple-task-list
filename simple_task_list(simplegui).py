defe530a8644986f2b45dd9e0b1cd76e5c2dbfeb
Stephen <whippetlegs@gmail.com>
2013-05-23 17:11:28 +0000

    Initial commit

---
+++ simple_task_list(simplegui).py
@@ -0,0 +1,12 @@
+# Simple task list
+# Currently uses simplegui module which is only available when using
+# http://www.codeskulptor.org
+
+import simplegui
+
+tasks = []
+
+# Handler for button
+def clear():
+    global tasks
+    tasks = []
